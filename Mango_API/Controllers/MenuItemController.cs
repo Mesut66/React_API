62bb5a7171bc66ba9f5f2292bcd845fbc9c60c3d
mesut yiğit <genmes-66@hotmail.com>
2025-05-06 15:44:08 +0300

    MenuItem Eklendi

---
+++ Mango_API/Controllers/MenuItemController.cs
@@ -0,0 +1,26 @@
+﻿using Azure;
+using Mango_API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Mango_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            return Ok(_db.MenuItems);
+        }
+    }
+}
